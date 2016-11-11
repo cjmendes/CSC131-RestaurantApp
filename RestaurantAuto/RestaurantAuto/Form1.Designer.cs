dafff98f8eea2e401f3d0e6c4c01f822090ace3a
Chris <Chris@DESKTOP-1G3O9OE>
2016-11-10 19:02:17 -0800

    The Log in part is about 80% done

--- RestaurantAuto/RestaurantAuto/Form1.Designer.cs
+++ RestaurantAuto/RestaurantAuto/Form1.Designer.cs
@@ -30,16 +30,26 @@
         {
             this.num1But = new System.Windows.Forms.Button();
             this.num2But = new System.Windows.Forms.Button();
-            this.lblLogInput = new System.Windows.Forms.Label();
+            this.lblLogInput1 = new System.Windows.Forms.Label();
             this.num3But = new System.Windows.Forms.Button();
             this.num4But = new System.Windows.Forms.Button();
             this.num5But = new System.Windows.Forms.Button();
+            this.lblLogInput2 = new System.Windows.Forms.Label();
+            this.lblLogInput3 = new System.Windows.Forms.Label();
+            this.lblLogInput4 = new System.Windows.Forms.Label();
+            this.num6But = new System.Windows.Forms.Button();
+            this.num7But = new System.Windows.Forms.Button();
+            this.num8But = new System.Windows.Forms.Button();
+            this.num9But = new System.Windows.Forms.Button();
+            this.num0But = new System.Windows.Forms.Button();
+            this.btnBackspace = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // num1But
             // 
             this.num1But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num1But.Location = new System.Drawing.Point(295, 176);
+            this.num1But.Location = new System.Drawing.Point(302, 136);
             this.num1But.Name = "num1But";
             this.num1But.Size = new System.Drawing.Size(53, 53);
             this.num1But.TabIndex = 0;
@@ -50,7 +60,7 @@
             // num2But
             // 
             this.num2But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num2But.Location = new System.Drawing.Point(365, 176);
+            this.num2But.Location = new System.Drawing.Point(370, 136);
             this.num2But.Name = "num2But";
             this.num2But.Size = new System.Drawing.Size(53, 53);
             this.num2But.TabIndex = 1;
@@ -58,20 +68,21 @@
             this.num2But.UseVisualStyleBackColor = true;
             this.num2But.Click += new System.EventHandler(this.num2But_Click);
             // 
-            // lblLogInput
+            // lblLogInput1
             // 
-            this.lblLogInput.AutoSize = true;
-            this.lblLogInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogInput.Location = new System.Drawing.Point(319, 106);
-            this.lblLogInput.Name = "lblLogInput";
-            this.lblLogInput.Size = new System.Drawing.Size(109, 39);
-            this.lblLogInput.TabIndex = 2;
-            this.lblLogInput.Text = " - - - -";
+            this.lblLogInput1.AutoSize = true;
+            this.lblLogInput1.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogInput1.Location = new System.Drawing.Point(320, 67);
+            this.lblLogInput1.Name = "lblLogInput1";
+            this.lblLogInput1.Size = new System.Drawing.Size(30, 39);
+            this.lblLogInput1.TabIndex = 2;
+            this.lblLogInput1.Text = "-";
             // 
             // num3But
             // 
             this.num3But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num3But.Location = new System.Drawing.Point(432, 176);
+            this.num3But.Location = new System.Drawing.Point(438, 136);
             this.num3But.Name = "num3But";
             this.num3But.Size = new System.Drawing.Size(53, 53);
             this.num3But.TabIndex = 3;
@@ -82,7 +93,7 @@
             // num4But
             // 
             this.num4But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num4But.Location = new System.Drawing.Point(295, 242);
+            this.num4But.Location = new System.Drawing.Point(302, 202);
             this.num4But.Name = "num4But";
             this.num4But.Size = new System.Drawing.Size(53, 53);
             this.num4But.TabIndex = 4;
@@ -93,7 +104,7 @@
             // num5But
             // 
             this.num5But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num5But.Location = new System.Drawing.Point(365, 243);
+            this.num5But.Location = new System.Drawing.Point(370, 202);
             this.num5But.Name = "num5But";
             this.num5But.Size = new System.Drawing.Size(53, 53);
             this.num5But.TabIndex = 5;
@@ -101,16 +112,133 @@
             this.num5But.UseVisualStyleBackColor = true;
             this.num5But.Click += new System.EventHandler(this.num5But_Click);
             // 
+            // lblLogInput2
+            // 
+            this.lblLogInput2.AutoSize = true;
+            this.lblLogInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogInput2.Location = new System.Drawing.Point(363, 67);
+            this.lblLogInput2.Name = "lblLogInput2";
+            this.lblLogInput2.Size = new System.Drawing.Size(30, 39);
+            this.lblLogInput2.TabIndex = 6;
+            this.lblLogInput2.Text = "-";
+            // 
+            // lblLogInput3
+            // 
+            this.lblLogInput3.AutoSize = true;
+            this.lblLogInput3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogInput3.Location = new System.Drawing.Point(406, 67);
+            this.lblLogInput3.Name = "lblLogInput3";
+            this.lblLogInput3.Size = new System.Drawing.Size(30, 39);
+            this.lblLogInput3.TabIndex = 7;
+            this.lblLogInput3.Text = "-";
+            // 
+            // lblLogInput4
+            // 
+            this.lblLogInput4.AutoSize = true;
+            this.lblLogInput4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogInput4.Location = new System.Drawing.Point(449, 67);
+            this.lblLogInput4.Name = "lblLogInput4";
+            this.lblLogInput4.Size = new System.Drawing.Size(30, 39);
+            this.lblLogInput4.TabIndex = 8;
+            this.lblLogInput4.Text = "-";
+            // 
+            // num6But
+            // 
+            this.num6But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num6But.Location = new System.Drawing.Point(438, 202);
+            this.num6But.Name = "num6But";
+            this.num6But.Size = new System.Drawing.Size(53, 53);
+            this.num6But.TabIndex = 9;
+            this.num6But.Text = "6";
+            this.num6But.UseVisualStyleBackColor = true;
+            this.num6But.Click += new System.EventHandler(this.num6But_Click);
+            // 
+            // num7But
+            // 
+            this.num7But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num7But.Location = new System.Drawing.Point(302, 270);
+            this.num7But.Name = "num7But";
+            this.num7But.Size = new System.Drawing.Size(53, 53);
+            this.num7But.TabIndex = 10;
+            this.num7But.Text = "7";
+            this.num7But.UseVisualStyleBackColor = true;
+            this.num7But.Click += new System.EventHandler(this.num7But_Click);
+            // 
+            // num8But
+            // 
+            this.num8But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num8But.Location = new System.Drawing.Point(370, 270);
+            this.num8But.Name = "num8But";
+            this.num8But.Size = new System.Drawing.Size(53, 53);
+            this.num8But.TabIndex = 11;
+            this.num8But.Text = "8";
+            this.num8But.UseVisualStyleBackColor = true;
+            this.num8But.Click += new System.EventHandler(this.num8But_Click);
+            // 
+            // num9But
+            // 
+            this.num9But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num9But.Location = new System.Drawing.Point(438, 270);
+            this.num9But.Name = "num9But";
+            this.num9But.Size = new System.Drawing.Size(53, 53);
+            this.num9But.TabIndex = 12;
+            this.num9But.Text = "9";
+            this.num9But.UseVisualStyleBackColor = true;
+            this.num9But.Click += new System.EventHandler(this.num9But_Click);
+            // 
+            // num0But
+            // 
+            this.num0But.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num0But.Location = new System.Drawing.Point(370, 338);
+            this.num0But.Name = "num0But";
+            this.num0But.Size = new System.Drawing.Size(53, 53);
+            this.num0But.TabIndex = 13;
+            this.num0But.Text = "0";
+            this.num0But.UseVisualStyleBackColor = true;
+            this.num0But.Click += new System.EventHandler(this.num0But_Click);
+            // 
+            // btnBackspace
+            // 
+            this.btnBackspace.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackspace.Location = new System.Drawing.Point(438, 338);
+            this.btnBackspace.Name = "btnBackspace";
+            this.btnBackspace.Size = new System.Drawing.Size(53, 53);
+            this.btnBackspace.TabIndex = 14;
+            this.btnBackspace.Text = "<";
+            this.btnBackspace.UseVisualStyleBackColor = true;
+            this.btnBackspace.Click += new System.EventHandler(this.btnBackspace_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(302, 338);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(53, 53);
+            this.btnEnter.TabIndex = 15;
+            this.btnEnter.Text = "E";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(810, 454);
+            this.Controls.Add(this.lblLogInput1);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnBackspace);
+            this.Controls.Add(this.num0But);
+            this.Controls.Add(this.num9But);
+            this.Controls.Add(this.num8But);
+            this.Controls.Add(this.num7But);
+            this.Controls.Add(this.num6But);
+            this.Controls.Add(this.lblLogInput4);
+            this.Controls.Add(this.lblLogInput3);
+            this.Controls.Add(this.lblLogInput2);
             this.Controls.Add(this.num5But);
             this.Controls.Add(this.num4But);
             this.Controls.Add(this.num3But);
-            this.Controls.Add(this.lblLogInput);
             this.Controls.Add(this.num2But);
             this.Controls.Add(this.num1But);
             this.Name = "Form1";
@@ -124,10 +252,20 @@
 
         private System.Windows.Forms.Button num1But;
         private System.Windows.Forms.Button num2But;
-        private System.Windows.Forms.Label lblLogInput;
+        private System.Windows.Forms.Label lblLogInput1;
         private System.Windows.Forms.Button num3But;
         private System.Windows.Forms.Button num4But;
         private System.Windows.Forms.Button num5But;
+        private System.Windows.Forms.Label lblLogInput2;
+        private System.Windows.Forms.Label lblLogInput3;
+        private System.Windows.Forms.Label lblLogInput4;
+        private System.Windows.Forms.Button num6But;
+        private System.Windows.Forms.Button num7But;
+        private System.Windows.Forms.Button num8But;
+        private System.Windows.Forms.Button num9But;
+        private System.Windows.Forms.Button num0But;
+        private System.Windows.Forms.Button btnBackspace;
+        private System.Windows.Forms.Button btnEnter;
     }
 }
 
--- RestaurantAuto/RestaurantAuto/Form1.cs
+++ RestaurantAuto/RestaurantAuto/Form1.cs
@@ -21,53 +21,177 @@ namespace RestaurantAuto
             InitializeComponent();
         }
 
+        /********************************************
+         ***               BUTTONS                ***
+         ********************************************/
         private void num1But_Click(object sender, EventArgs e)
         {
-            logIn[logInPosition] = 1;
-            logInPosition++;
-            refreshLogIn();
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 1;
+                logInPosition++;
+                refreshLogIn();
+            }
         }
 
         private void num2But_Click(object sender, EventArgs e)
         {
-            logIn[logInPosition] = 2;
-            logInPosition++;
-            refreshLogIn();
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 2;
+                logInPosition++;
+                refreshLogIn();
+            }
         }
 
         private void num3But_Click(object sender, EventArgs e)
         {
-            logIn[logInPosition] = 3;
-            logInPosition++;
-            refreshLogIn();
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 3;
+                logInPosition++;
+                refreshLogIn();
+            }
         }
 
         private void num4But_Click(object sender, EventArgs e)
         {
-            logIn[logInPosition] = 4;
-            logInPosition++;
-            refreshLogIn();
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 4;
+                logInPosition++;
+                refreshLogIn();
+            }
         }
 
         private void num5But_Click(object sender, EventArgs e)
         {
-            logIn[logInPosition] = 5;
-            logInPosition++;
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 5;
+                logInPosition++;
+                refreshLogIn();
+            }
+        }
+
+        private void num6But_Click(object sender, EventArgs e)
+        {
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 6;
+                logInPosition++;
+                refreshLogIn();
+            }
+        }
+
+        private void num7But_Click(object sender, EventArgs e)
+        {
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 7;
+                logInPosition++;
+                refreshLogIn();
+            }
+        }
+
+        private void num8But_Click(object sender, EventArgs e)
+        {
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 8;
+                logInPosition++;
+                refreshLogIn();
+            }
+        }
+
+        private void num9But_Click(object sender, EventArgs e)
+        {
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 9;
+                logInPosition++;
+                refreshLogIn();
+            }
+        }
+
+        private void num0But_Click(object sender, EventArgs e)
+        {
+            if (logInPosition < 4)
+            {
+                logIn[logInPosition] = 0;
+                logInPosition++;
+                refreshLogIn();
+            }
+        }
+
+        private void btnBackspace_Click(object sender, EventArgs e)
+        {
+            if (logInPosition > 0) { 
+                if (logInPosition == 1)
+                    lblLogInput1.Text = "-";
+                else if (logInPosition == 2)
+                    lblLogInput2.Text = "-";
+                else if (logInPosition == 3)
+                    lblLogInput3.Text = "-";
+                else if (logInPosition == 4)
+                    lblLogInput4.Text = "-";
+                logInPosition--;
+            }
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (logInPosition < 4)
+                clearLogIn();
+            else
+                checkPassword();
+        }
+
+
+        /********************************************
+         ***              FUNCTIONS               ***
+         ********************************************/
+
+        private void clearLogIn()
+        {
+            for (int i = 0; i < logIn.Length; i++)
+                logIn[i] = -1;
+            logInPosition = 0;
             refreshLogIn();
+        }
+
+        private void checkPassword()
+        {
+            string tempPass = "" + logIn[0] + logIn[1] + logIn[2] + logIn[3];
+            if (logInPassword == tempPass)
+                tempPass = "done";
+            else
+                clearLogIn();
         }
 
         private void refreshLogIn()
         {
-            string input = "";
-            for(int i = 0; i < logIn.Length; i++)
-            {
-                if(logIn[i] == -1)
-                    input += " -";
-                else
-                    input = input + " " + logIn[i] + " ";
-            }
-            input += " ";
-            lblLogInput.Text = input;
+
+            if (logIn[0] == -1)
+                lblLogInput1.Text = "-";
+            else
+                lblLogInput1.Text = "" + logIn[0];
+
+            if (logIn[1] == -1)
+                lblLogInput2.Text = "-";
+            else
+                lblLogInput2.Text = "" + logIn[1];
+
+            if (logIn[2] == -1)
+                lblLogInput3.Text = "-";
+            else
+                lblLogInput3.Text = "" + logIn[2];
+
+            if (logIn[3] == -1)
+                lblLogInput4.Text = "-";
+            else
+                lblLogInput4.Text = "" + logIn[3];
         }
+
     }
 }
